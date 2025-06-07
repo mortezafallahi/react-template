08dae89014b3302f8538d2d060511b45012094e3
morteza <mortezafallahi1385@gmail.com>
2025-06-07 18:04:42 +0330

    add biome and remove prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './styles/index.scss';
-import App from './App.jsx';
-import { HeroUIProvider } from '@heroui/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/index.scss";
+import { HeroUIProvider } from "@heroui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.jsx";
 const queryClient = new QueryClient();
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <HeroUIProvider locale='fa-IR'>
+      <HeroUIProvider locale="fa-IR">
         <App />
       </HeroUIProvider>
     </QueryClientProvider>
